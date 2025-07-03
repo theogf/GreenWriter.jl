bd1115efa150fd3460ba9a2a0ec5d95ca1b8eadd
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2025-07-03 22:54:21 +0200

    nice little things

--- src/GreenWriter.jl
+++ src/GreenWriter.jl
@@ -2,7 +2,7 @@ module GreenWriter
 
 using AbstractTrees
 using JuliaSyntax
-using JuliaSyntax: @K_str, GreenNode, parseall, span, SyntaxHead, head, is_leaf
+using JuliaSyntax: @K_str, GreenNode, parsestmt, span, SyntaxHead, head, is_leaf
 
 export GreenText, print_tree
 
@@ -19,12 +19,14 @@ AbstractTrees.children(node::GreenText) = node.children
 AbstractTrees.nodevalue(node::GreenText) = isnothing(node.content) ? node.head.kind : node.content
 Base.show(io::IO, ::MIME"text/plain", node::GreenText) = print_tree(io, node)
 Base.getindex(node::GreenText, i::Integer) = node.children[i]
+Base.lastindex(node::GreenText) = length(node.children)
 Base.push!(node::GreenText, child::GreenText) = push!(node.children, child)
+Base.push!(node::GreenText, text::AbstractString) = push!(node, GreenText(text))
 JuliaSyntax.kind(node::GreenText) = node.head
 JuliaSyntax.is_leaf(node::GreenText) = isempty(node.children)
 
 function Base.parse(::Type{GreenText}, text::AbstractString)
-  green_tree = parseall(GreenNode, text)
+  green_tree = parsestmt(GreenNode, text)
   last(fetch_node_text(green_tree, text, 1)) 
 end
 
