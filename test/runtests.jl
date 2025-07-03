eaa7a458209999d0b05d633670e1f7cb4cdd4240
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2025-07-03 22:54:29 +0200

    add a few test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,10 @@ using GreenWriter
 using Test
 
 @testset "GreenWriter.jl" begin
-    text = """
-    foo(a) = 2 + 1
-    """
+    text = "foo(a) = 2 + 1"
+    node = parse(GreenText, text)
+    @test string(node) == text
+    push!(node[end], " + 1")
+    text = "foo(a) = 2 + 1 + 1"
+    @test string(node) == text
 end
